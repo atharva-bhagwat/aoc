755c304388a4dc28f1e51d6396d9d09cc57b0314
Atharva Bhagwat <atharva.bhagwat42@gmail.com>
2022-01-01 20:02:26 -0500

    2021 Q7

---
+++ 2021/7/main.py
@@ -0,0 +1,35 @@
+import sys
+
+input = [int(x) for x in open(sys.argv[1],'r').read().strip().split(',')]
+
+high = max(input)
+
+def part1():
+    prev_cost = float('inf')
+    for ind in range(high+1):
+        cost = 0
+        for val in input:
+            cost += abs(ind-val)
+        if prev_cost > cost:
+            prev_cost = cost
+        else:
+            print(f'Part 1: {prev_cost}')
+            return
+
+def calc_sum(n):
+    return sum(list(range(n+1)))
+
+def part2():
+    prev_cost = float('inf')
+    for ind in range(high+1):
+        cost = 0
+        for val in input:
+            cost += calc_sum(abs(ind-val))
+        if prev_cost > cost:
+            prev_cost = cost
+        else:
+            print(f'Part 2: {prev_cost}')
+            return
+
+part1()
+part2()
